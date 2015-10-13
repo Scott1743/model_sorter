020157d4a4e3df74514042ad93549bcc85e9e694
Scott1743 <512981271@qq.com>
2015-10-13 23:23:43 +0800

    first commit

---
+++ lib/model/sorter.rb
@@ -0,0 +1,7 @@
+require "model/sorter/version"
+
+module Model
+  module Sorter
+    # Your code goes here...
+  end
+end
---
+++ lib/model/sorter/version.rb
@@ -0,0 +1,5 @@
+module Model
+  module Sorter
+    VERSION = "0.0.1"
+  end
+end
